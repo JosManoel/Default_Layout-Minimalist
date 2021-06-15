77d8a72a7c958f26188751f4426a7243fc69ecfe
JosManoel <josmanoelfreitas2012@gmail.com>
2021-06-15 20:54:48 -0300

    modified iframe height script

--- assets_page/scripts/iframe_height.js
+++ assets_page/scripts/iframe_height.js
@@ -1,31 +1,14 @@
 window.onload = function(){
-    firstLoadIframe = calcIframeHeight();
-    setTimeout(resizeIframe(),5000);
+    setTimeout(calcIframeHeight(),500);
 }
 
 window.onclick = function(){
-    resizeIframe();
+    calcIframeHeight();
 }
 
 function calcIframeHeight(){
-    return document.getElementById("iframePage").scrollHeight;
+    let iframe = document.getElementById("iframePage").scrollHeight;
+    let parentIframe = parent.document.getElementById("iframe");
+
+    parentIframe.height = iframe;
 }
-
-function resizeIframe(){
-    var iframeHeight = calcIframeHeight();
-    let parentHeight = parent.document.getElementById("iframe");
-    
-    if(parentHeight.height != iframeHeight){
-
-        if( parentHeight.height == firstLoadIframe){
-            parentHeight.height = iframeHeight;
-        }else{
-            parentHeight.height = firstLoadIframe;
-        }
-    }
- }
-
-
-// O Bug de resize do iframe acontece pq no teste a conexão nao é https,onde o chrome nao carrega completamente o iframe, diferente do firefox e do opera. por isso funciona neles e no chrome nao.
-
-//https://stackoverflow.com/questions/45117519/iframes-not-loading-on-mobile-or-tablet
